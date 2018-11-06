2ba92912d770b441185a9cb1f65b21b556758b16
Helle Gråberg <helle.graaberg@hotmail.com>
2018-11-06 20:43:18 +0100

    body dir

--- MyFirstPenguin/inFrontOfMe.py
+++ MyFirstPenguin/inFrontOfMe.py
@@ -22,67 +22,67 @@ def in_front_of_me(body):
                 if item['x'] == penguinPositionX:
                     item_y = item["y"]
                     if item_y > 0:
-                        my_list.append((penguinPositionY - item_y, "enemies"))
+                        my_list.append((abs(penguinPositionY - item_y), "enemies"))
         for item in walls:
             if item['x'] == penguinPositionX:
                 item_y = item["y"]
                 if item_y > 0:
-                    my_list.append((penguinPositionY - item_y, "walls"))
+                    my_list.append((abs(penguinPositionY - item_y), "walls"))
         for item in bonusTiles:
             if item['x'] == penguinPositionX:
                 item_y = item["y"]
                 if item_y > 0:
-                    my_list.append((penguinPositionY - item_y, "bonusTiles"))
+                    my_list.append((abs(penguinPositionY - item_y), "bonusTiles"))
     elif bodyDirection == "bottom":
         for item in enemies:
             if "x" in item.keys():
                 if item['x'] == penguinPositionX:
                     item_y = item["y"]
                     if item_y < 0:
-                        my_list.append((item_y - penguinPositionY, "enemies"))
+                        my_list.append((abs(item_y - penguinPositionY), "enemies"))
         for item in walls:
             if item['x'] == penguinPositionX:
                 item_y = item["y"]
                 if item_y < 0:
-                    my_list.append((item_y - penguinPositionY, "walls"))
+                    my_list.append((abs(item_y - penguinPositionY), "walls"))
         for item in bonusTiles:
             if item['x'] == penguinPositionX:
                 item_y = item["y"]
                 if item_y < 0:
-                    my_list.append((item_y - penguinPositionY, "bonusTiles"))
+                    my_list.append((abs(item_y - penguinPositionY), "bonusTiles"))
     elif bodyDirection == "left":
         for item in enemies:
             if "x" in item.keys():
                 if item['y'] == penguinPositionY:
                     item_x = item["x"]
                     if item_x < 0:
-                        my_list.append((penguinPositionX - item_x, "enemies"))
+                        my_list.append((abs(penguinPositionX - item_x), "enemies"))
         for item in walls:
             if item['y'] == penguinPositionY:
                 item_x = item["x"]
                 if item_x < 0:
-                    my_list.append((penguinPositionX - item_x, "walls"))
+                    my_list.append((abs(penguinPositionX - item_x), "walls"))
         for item in bonusTiles:
             if item['y'] == penguinPositionX:
                 item_x = item["x"]
                 if item_x < 0:
-                    my_list.append((penguinPositionX - item_x, "bonusTiles"))
+                    my_list.append((abs(penguinPositionX - item_x), "bonusTiles"))
     elif bodyDirection == "right":
         for item in enemies:
             if "x" in item.keys():
                 if item['y'] == penguinPositionY:
                     item_x = item["x"]
                     if item_x > 0:
-                        my_list.append((item_x - penguinPositionX, "enemies"))
+                        my_list.append((abs(item_x - penguinPositionX), "enemies"))
         for item in walls:
             if item['y'] == penguinPositionY:
                 item_x = item["x"]
                 if item_x > 0:
-                    my_list.append((item_x - penguinPositionX, "walls"))
+                    my_list.append((abs(item_x - penguinPositionX), "walls"))
         for item in bonusTiles:
             if item['y'] == penguinPositionX:
                 item_x = item["x"]
                 if item_x > 0:
-                    my_list.append((item_x - penguinPositionX, "bonusTiles"))
+                    my_list.append((abs(item_x - penguinPositionX), "bonusTiles"))
 
     return sorted(my_list)
